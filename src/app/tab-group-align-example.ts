4b67ee0093cf63806de471454fb3e5d012cdd4ae
PriyaRani12 <priya.rani.cs.2015@mitmeerut.ac.in>
2020-08-26 15:38:18 +0530

    Initial commit

---
+++ src/app/tab-group-align-example.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Tab group with aligned labels
+ */
+@Component({
+  selector: 'tab-group-align-example',
+  templateUrl: 'tab-group-align-example.html',
+  styleUrls: ['tab-group-align-example.css'],
+})
+export class TabGroupAlignExample {}
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
